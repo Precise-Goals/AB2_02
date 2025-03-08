acddf4f24913fbf086bf63931ecdead61591490a
Precise Goals <elitesarthakamr@gmail.com>
2025-03-08 18:29:18 +0530

    some key requiremented changes

--- src/containers/EditorHeader.jsx
+++ src/containers/EditorHeader.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { db } from "../firebase";
+import { db } from "../Firebase";
 import {
   collection,
   addDoc,
